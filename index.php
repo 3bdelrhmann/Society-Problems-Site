233e1e3bc97595f71d61113c615f881107c5599f
Mohammed Ehab <modytiger123@yahoo.com>
2016-06-08 07:13:07 +0200

    Till the header section
    The project finished to the header section !

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+    <title>Society Problems</title>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="style.css" />
+    <meta charset="UTF-8">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+    <div class="row">
+        <div class="col s12 header center-align">
+            <div class="logo-container">
+                <img src="images/Logo.png" alt="Society Problems Logo !" />
+            </div>
+            <div class="col s12 navbar-container center-align">
+                <ul class="navbar" style='padding-bottom: 0 !important;margin-bottom: 0;'>
+                    <li><a href="#" class="waves-effect waves-light btn">الرئيسية</a></li>
+                    <li><a href="#" class="waves-effect waves-light btn">اضافة منشور جديد</a></li>
+                </ul>
+
+                <ul class="navbar">
+                    <li>
+                        <a href="#loginForm" class="waves-effect waves-light btn modal-trigger">دخول</a>
+                    </li>
+                    <li><a href="#" class="waves-effect waves-light btn">تسجيل</a></li>
+                    <li><a href="#" class="waves-effect waves-light btn">نبذة عن الموقع</a></li>
+                    <li><a href="#" class="waves-effect waves-light btn">الاقسام هنا</a></li>
+                    <li><a href="#" class="waves-effect waves-light btn">الاقسام هنا</a></li>
+                    <li><a href="#" class="waves-effect waves-light btn">الاقسام هنا</a></li>
+                </ul>
+                <div id="loginForm" class="modal modal-fixed-footer">
+                    <div class="modal-content">
+                        <h4>تسجيل الدخول</h4>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">دخول</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
