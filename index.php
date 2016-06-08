9d7eda59c96504312b5f982f67169d160c469566
Abdelrhman-Mohamed <Abdelrhman_M@outlook.com>
2016-06-08 16:59:35 +0200

    Finish Header
    Finished Header and login register form

--- index.php
+++ index.php
@@ -36,53 +36,76 @@
                 </ul>
                 <div id="loginForm" class="modal modal-fixed-footer" style="width: 70%;">
                     <div class="modal-content">
+                      <div class="header-modal">
                         <h5 class="darkblue">تسجيل / تسجيل الدخول</h5>
-                        <form class="register col l6 m12" style="border-right: 1px solid #2D3E52;">
-                            <h6 style="font-family: Droid-Bold;" class="darkblue">تسجيل مستخدم جديد</h6>
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input type='text' class="validate" id="first-name" />
-                                    <label for="first-name">الاسم الكامل</label>
-                                </div>
+                      </div>
+                        <form class=" fl-left register col l6 m12" style="border-right: 1px solid #2D3E52;">
+                            <div class="header-register">
+                              <h6 style="font-family: Droid-Bold;" class="darkblue">تسجيل مستخدم جديد</h6>
                             </div>
+                            <div class="content-register">
+                              <div class="row">
+                                  <div class="input-field col s12">
+                                    <label for="FnameLname">الأسم الاول والاخير</label>
+                                      <input required type='text' class="validate" id="FnameLname" />
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="input-field col s12">
+                                    <label for="Username">اسم المستخدم</label>
+                                      <input required type='text' class="validate" id="Username" />
+                                  </div>
+                              </div>
 
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input type='email' class="validate" id="email" />
-                                    <label for="email">البريد الالكتروني</label>
-                                </div>
-                            </div>
+                              <div class="row">
+                                  <div class="input-field col s12">
+                                    <label for="email-reg">البريد الالكتروني</label>
+                                      <input required type='email' class="validate" id="email-reg" />
+                                  </div>
+                              </div>
 
 
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input type='password' class="validate" id="password" />
-                                    <label for="password">كلمة المرور</label>
-                                </div>
+                              <div class="row">
+                                  <div class="input-field col s12">
+                                    <label for="password-reg">كلمة المرور</label>
+                                      <input required type='password' class="validate" id="password-reg" />
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="input-field col s12">
+                                      <input  type='submit' class="waves-effect waves-light btn" value="تسجيل" />
+                                  </div>
+                              </div>
                             </div>
                         </form>
 
-                        <form class="login col l6 m12">
+                        <form class="fl-right login col l6 m12">
                             <h6 style="font-family: Droid-Bold;" class="darkblue">تسجيل دخول</h6>
-
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input type='email' class="validate" id="email" />
-                                    <label for="email">البريد الالكتروني</label>
+                              <div class="content-login">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                      <label for="email-log">البريد الالكتروني او اسم المستخدم</label>
+                                        <input required type='email' class="validate" id="email-log" />
+                                    </div>
                                 </div>
-                            </div>
 
 
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <input type='password' class="validate" id="password" />
-                                    <label for="password">كلمة المرور</label>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                      <label for="password-log">كلمة المرور</label>
+                                        <input required type='password' class="validate" id="password-log" />
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <input  type='submit' class="waves-effect waves-light btn" value="تسجيل الدخول" />
+                                    </div>
+                                </div>
+                              </div>
                         </form>
                     </div>
                     <div class="modal-footer ">
-                        <a href="#! " class=" modal-action modal-close waves-effect waves-green btn-flat ">دخول</a>
+                        <a href="#! " class=" modal-action modal-close waves-effect waves-green btn-flat ">خروج</a>
                     </div>
                 </div>
             </div>
