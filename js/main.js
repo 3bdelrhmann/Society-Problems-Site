f66f09dac5e2a0aa1695bcb735f9a277a42b4490
Mohammed Ehab <modytiger123@yahoo.com>
2016-06-13 03:03:25 +0200

    Refreshed All

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-  $(document).ready(function () {
-      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-      $('.modal-trigger').leanModal({
-          dismissible: true
-      });
+/*global $*/
 
-      $('.dropdownToggle').click(function () {
-          $('.dropdown').slideToggle(300);
-      });
+$(document).ready(function () {
+    'use strict';
 
-      $('.mobile-toggle').click(function () {
-          $('.navbar').slideToggle();
-          $('.dropdown').slideUp();
-      });
-  });
+    $('select').material_select();
+
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal({
+        dismissible: true
+    });
+
+    $('.dropdownToggle').click(function () {
+        $('.dropdown').slideToggle(300);
+    });
+
+    $('.mobile-toggle').click(function () {
+        $('.navbar').slideToggle();
+        $('.dropdown').slideUp();
+    });
+
+    $('.dropdown-content').parent().css('position', 'relative');
+    $('.select-dropdown').parent().css('position', 'relative');
+
+    $('.dropdown-button').click(function () {
+        $('.dropdown-content[id=' + $(this).attr('data-activates') + ']').css('top', $(this).parent().css('height'));
+    });
+
+});
